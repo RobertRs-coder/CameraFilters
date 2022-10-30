258242644f34759e6252c1c2606eecf3a64afe97
Roberto Rojo <rrojo.va@gmail.com>
2022-10-30 17:56:03 +0100

    Initial Commit

---
+++ CameraFilters/CameraFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraFiltersApp.swift
+//  CameraFilters
+//
+//  Created by Roberto Rojo Sahuquillo on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
