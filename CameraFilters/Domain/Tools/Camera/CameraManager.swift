914446e42d2de5a1e529c56a06cb0e6b18f9ffa5
Roberto Rojo <rrojo.va@gmail.com>
2022-10-30 20:48:39 +0100

    New CameraManager attributes to manage errors, camera status & AVCaptureSession

--- CameraFilters/Domain/Tools/Camera/CameraManager.swift
+++ CameraFilters/Domain/Tools/Camera/CameraManager.swift
@@ -11,6 +11,14 @@ class CameraManager: ObservableObject {
 
     static let shared = CameraManager()
 
+    @Published var error: CameraError?
+    
+    let session = AVCaptureSession()
+    
+    private let sessionQueue = DispatchQueue(label: "com.robertrs.SessionQ")
+    private let videoOutput = AVCaptureVideoDataOutput()
+    private var status = CameraStatus.unconfigured
+    
     private init() {
         configure()
     }
