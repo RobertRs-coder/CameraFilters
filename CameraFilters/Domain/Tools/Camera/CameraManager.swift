d4aaa3a265031adcf92f82a81a15c7eaa6c48a09
Roberto Rojo <rrojo.va@gmail.com>
2022-10-30 21:31:05 +0100

    Add to Camera Manager a method to check permissions

--- CameraFilters/Domain/Tools/Camera/CameraManager.swift
+++ CameraFilters/Domain/Tools/Camera/CameraManager.swift
@@ -8,9 +8,9 @@
 import AVFoundation
 
 class CameraManager: ObservableObject {
-
+    
     static let shared = CameraManager()
-
+    
     @Published var error: CameraError?
     
     let session = AVCaptureSession()
@@ -22,43 +22,43 @@ class CameraManager: ObservableObject {
     private init() {
         configure()
     }
-
+    
     private func configure() {
     }
     
     private func set(error: CameraError?) {
-      DispatchQueue.main.async {
-        self.error = error
-      }
+        DispatchQueue.main.async {
+            self.error = error
+        }
     }
     
     private func checkPermissions() {
-      switch AVCaptureDevice.authorizationStatus(for: .video) {
-      case .notDetermined:
-        sessionQueue.suspend()
-        AVCaptureDevice.requestAccess(for: .video) { authorized in
-          
-          if !authorized {
-            self.status = .unauthorized
-            self.set(error: .deniedAuthorization)
-          }
-          self.sessionQueue.resume()
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video) { authorized in
+                
+                if !authorized {
+                    self.status = .unauthorized
+                    self.set(error: .deniedAuthorization)
+                }
+                self.sessionQueue.resume()
+            }
+        case .restricted:
+            status = .unauthorized
+            set(error: .restrictedAuthorization)
+        case .denied:
+            status = .unauthorized
+            set(error: .deniedAuthorization)
+        case .authorized:
+            break
+            
+        @unknown default:
+            status = .unauthorized
+            set(error: .unknownAuthorization)
         }
-      case .restricted:
-        status = .unauthorized
-        set(error: .restrictedAuthorization)
-      case .denied:
-        status = .unauthorized
-        set(error: .deniedAuthorization)
-      case .authorized:
-        break
-    
-      @unknown default:
-        status = .unauthorized
-        set(error: .unknownAuthorization)
-      }
     }
-
-
+    
+    
     
 }
