566e2fb2533a8310586c47c911c1ad21b49c1a55
Roberto Rojo <rrojo.va@gmail.com>
2022-10-30 18:12:37 +0100

    Create some basics views to start the project

--- CameraFilters/UI/Home/Views/ContentView.swift
+++ CameraFilters/UI/Home/Views/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack {
+      Color.black.edgesIgnoringSafeArea(.all)
+
+      Text("Filter the World!")
+        .foregroundColor(.white)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
