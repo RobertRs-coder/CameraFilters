97d054a0b9fdcdc8b992b7218bcd969510d90267
Roberto Rojo <rrojo.va@gmail.com>
2022-10-30 22:18:00 +0100

    Create & configure RootViewModel to manage the data and paint the views with camera images

---
+++ CameraFilters/UI/Root/ViewModels/RootViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewModel.swift
+//  CameraFilters
+//
+//  Created by Roberto Rojo Sahuquillo on 30/10/22.
+//
+
+import CoreImage
+//The view model will then feed your view all the data it needs to display what you want
+class ContentViewModel: ObservableObject {
+  // 1 frame will hold the images that FrameView will display.
+  @Published var frame: CGImage?
+  // 2 Data used to generate frame will come from FrameManager.
+  private let frameManager = FrameManager.shared
+
+  init() {
+    setupSubscriptions()
+  }
+  // 3 You’ll add all your Combine pipelines to setupSubscriptions() to keep them in one place.
+  func setupSubscriptions() {
+      //To transform the CVPixelBuffer data FrameManager provides to a CGImage your FrameView
+      
+      // 1 Tap into the Publisher that was automatically created for you when you used @Published.
+      frameManager.$current
+        // 2 Receive the data on the main run loop. It should already be on main, but just in case, it doesn’t hurt to be sure.
+        .receive(on: RunLoop.main)
+        // 3 Convert CVPixelBuffer to CGImage and filter out all nil values through compactMap.
+        .compactMap { buffer in
+          return CGImage.create(from: buffer)
+        }
+        // 4 Assign the output of the pipeline — which is, itself, a publisher — to your published frame.
+        .assign(to: &$frame)
+
+  }
+}
